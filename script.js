38f5d99f041d66cc63dc4d836327754cf5bba12b
rajeshycode <136836225+rajeshycode@users.noreply.github.com>
2023-09-24 16:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function decimalToBinary(num) {
-  //Write you code here
+  // Check if the input is not a valid positive integer
+  if (!Number.isInteger(num) || num < 0) {
+    return "Invalid input. Please enter a positive integer.";
+  }
+
+  // Handle the special case when num is 0
+  if (num === 0) {
+    return "0";
+  }
+
+  let binary = "";
   
+  while (num > 0) {
+    // Get the remainder when dividing by 2 (0 or 1)
+    const remainder = num % 2;
+    // Add the remainder to the beginning of the binary representation
+    binary = remainder + binary;
+    // Divide the number by 2, rounding down
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
